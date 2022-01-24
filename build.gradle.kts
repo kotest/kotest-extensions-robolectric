b8a133c029484d23d6a1838e7ae4ca2218607e4f
matsudamper <let@matsudamper.net>
2022-01-24 11:55:51 +0900

    update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,4 +43,13 @@ allprojects {
          url = uri("https://oss.sonatype.org/content/repositories/snapshots")
       }
    }
+
+   tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+      kotlinOptions {
+         freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
+         jvmTarget = "1.8"
+         apiVersion = "1.6"
+         languageVersion = "1.6"
+      }
+   }
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,11 +1,11 @@
 object Libs {
 
-   const val kotlinVersion = "1.4.31"
+   const val kotlinVersion = "1.6.10"
    const val org = "io.kotest.extensions"
    const val artifactId = "kotest-extensions-robolectric"
 
    object Kotest {
-      private const val version = "4.6.3"
+      private const val version = "5.1.0"
       const val api = "io.kotest:kotest-framework-api:$version"
       const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
    }
@@ -24,7 +24,7 @@ object Libs {
       const val compileSdkVersion = 30
       const val targetSdkVersion = 30
       const val minSdkVersion = 14
-      const val androidGradlePlugin = "com.android.tools.build:gradle:4.2.1"
+      const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.4"
       const val appcompat = "androidx.appcompat:appcompat:1.3.0"
       const val material = "com.google.android.material:material:1.3.0"
       const val multidex = "androidx.multidex:multidex:2.0.1"
